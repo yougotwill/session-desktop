29a32d774771f9de650b115f769eeb995fbf7f5b
William Grant <willmgrant@gmail.com>
2022-07-14 10:16:50 +1000

    fix: pass yarn ready

--- ts/components/dialog/ReactClearAllModal.tsx
+++ ts/components/dialog/ReactClearAllModal.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { updateReactClearAllModal } from '../../state/ducks/modalDialog';
