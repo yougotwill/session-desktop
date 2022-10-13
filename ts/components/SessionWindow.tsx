9a12cbb294b585ae1e4de8f60f73bc920091dbe6
William Grant <willmgrant@gmail.com>
2022-10-13 17:55:52 +1100

    fix: improved windows titlebar style

--- ts/components/SessionWindow.tsx
+++ ts/components/SessionWindow.tsx
@@ -1,21 +1,24 @@
 import React, { ReactNode } from 'react';
 import styled from 'styled-components';
+import { isMacOS } from '../OS';
 import { Flex } from './basic/Flex';
 
+// TODO Need to account for linux
 export const titleBarHeight = 28;
 
-const StyledTitleBarSpace = styled.div`
-  width: 100%;
+const StyledTitleBarSpace = styled.div<{ isMac: boolean }>`
+  width: 100vw;
   height: ${titleBarHeight}px;
-  background-color: var(--background-primary-color);
+  ${props => props.isMac && 'background-color: var(--background-primary-color);'}
   border-bottom: 1px solid var(--border-color);
   -webkit-app-region: drag;
 `;
 
 export const SessionWindow = ({ children }: { children: ReactNode }) => {
+  const isMac = isMacOS();
   return (
     <Flex container={true} flexDirection={'column'}>
-      <StyledTitleBarSpace />
+      <StyledTitleBarSpace isMac={isMac} />
       {children}
     </Flex>
   );
