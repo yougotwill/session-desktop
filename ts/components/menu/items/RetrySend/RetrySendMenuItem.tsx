53a6dae896e44e38453e68b54ee66f6d4179992b
Audric Ackermann <audric@getsession.org>
2024-12-11 16:41:55 +1100

    chore: move deleteMessage & retrySend to other files

---
+++ ts/components/menu/items/RetrySend/RetrySendMenuItem.tsx
@@ -0,0 +1,25 @@
+import { useCallback } from 'react';
+import { WithMessageId } from '../../../../session/types/with';
+import { useMessageDirection, useMessageStatus } from '../../../../state/selectors';
+import { ItemWithDataTestId } from '../MenuItemWithDataTestId';
+import { Data } from '../../../../data/data';
+
+export const RetryItem = ({ messageId }: WithMessageId) => {
+  const direction = useMessageDirection(messageId);
+
+  const status = useMessageStatus(messageId);
+  const isOutgoing = direction === 'outgoing';
+
+  const showRetry = status === 'error' && isOutgoing;
+  const onRetry = useCallback(async () => {
+    const found = await Data.getMessageById(messageId);
+    if (found) {
+      await found.retrySend();
+    }
+  }, [messageId]);
+
+  return showRetry ? (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <ItemWithDataTestId onClick={onRetry}>{window.i18n('resend')}</ItemWithDataTestId>
+  ) : null;
+};
