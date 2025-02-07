010801b78ca30c597f861c887aab59c1f350528a
Audric Ackermann <audric@getsession.org>
2025-02-07 16:12:21 +1100

    fix: allow to switch from create group/groupv2 based on a ts.flag

--- ts/state/ducks/releasedFeatures.tsx
+++ ts/state/ducks/releasedFeatures.tsx
@@ -1,8 +1,14 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import { DURATION } from '../../session/constants';
 
+// update this to be when we ship desktop groups REMOVE AFTER QA
+const GROUP_DESKTOP_RELEASE = 1767225600 * 1000; // currently  Thursday, January 1, 2026 12:00:00 AM
+
 // FIXME update this to the correct timestamp REMOVE AFTER QA
-export const LEGACY_GROUP_DEPRECATED_TIMESTAMP_MS = Date.now() + DURATION.WEEKS * 52;
+export const START_CREATE_NEW_GROUP_TIMESTAMP_MS = GROUP_DESKTOP_RELEASE + DURATION.WEEKS * 1;
+
+// FIXME update this to the correct timestamp REMOVE AFTER QA
+export const LEGACY_GROUP_DEPRECATED_TIMESTAMP_MS = GROUP_DESKTOP_RELEASE + DURATION.WEEKS * 3;
 
 export interface ReleasedFeaturesState {
   legacyGroupDeprecationTimestampRefreshAtMs: number;
--- ts/state/selectors/releasedFeatures.ts
+++ ts/state/selectors/releasedFeatures.ts
@@ -1,11 +1,25 @@
+import { useSelector } from 'react-redux';
 import { NetworkTime } from '../../util/NetworkTime';
-import { LEGACY_GROUP_DEPRECATED_TIMESTAMP_MS } from '../ducks/releasedFeatures';
+import {
+  LEGACY_GROUP_DEPRECATED_TIMESTAMP_MS,
+  START_CREATE_NEW_GROUP_TIMESTAMP_MS,
+} from '../ducks/releasedFeatures';
 
 export const areLegacyGroupsDeprecatedYet = (): boolean => {
   const theyAreDeprecated = NetworkTime.now() >= LEGACY_GROUP_DEPRECATED_TIMESTAMP_MS;
 
   return window.sessionFeatureFlags.forceLegacyGroupsDeprecated || theyAreDeprecated;
 };
+
+const areGroupsCreatedAsNewGroupsYet = (): boolean => {
+  const shouldCreateNewGroups = NetworkTime.now() >= START_CREATE_NEW_GROUP_TIMESTAMP_MS;
+
+  return window.sessionFeatureFlags.useClosedGroupV2 || shouldCreateNewGroups;
+};
+
+export function useAreGroupsCreatedAsNewGroupsYet() {
+  return useSelector(areGroupsCreatedAsNewGroupsYet);
+}
 
 export function areLegacyGroupsDeprecatedYetOutsideRedux() {
   if (!window.inboxStore) {
