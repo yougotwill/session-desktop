ff85660aebc19c3d37b76238f54ec1bcafd99894
yougotwill <yougotwill94@gmail.com>
2025-02-07 16:32:27 +1100

    fix: combine sanitizeDisplayNameOrToast and displayNameIsValid into one function

--- ts/components/registration/utils/index.tsx
+++ ts/components/registration/utils/index.tsx
@@ -1,37 +1,12 @@
-import { Dispatch } from '@reduxjs/toolkit';
+import { EmptyDisplayNameError } from '../../../session/utils/errors';
 import { sanitizeSessionUsername } from '../../../session/utils/String';
 
-export function sanitizeDisplayNameOrToast(
-  displayName: string,
-  // can be a useState or redux function
-  onDisplayNameError: (error: string | undefined) => any,
-  dispatch?: Dispatch
-) {
-  const sanitizedName = sanitizeSessionUsername(displayName);
-  const errorString = !sanitizedName ? window.i18n('displayNameErrorDescription') : undefined;
-  if (dispatch) {
-    dispatch(onDisplayNameError(errorString));
-  } else {
-    onDisplayNameError(errorString); // this is is either calling dispatch in the caller or just `setDisplayNameError`
+export function sanitizeDisplayNameOrToast(displayName: string) {
+  const sanitizedName = sanitizeSessionUsername(displayName).trim();
+
+  if (!sanitizedName) {
+    throw new EmptyDisplayNameError();
   }
 
   return sanitizedName;
 }
-
-/**
- * Returns undefined if an error happened, or the trim userName.
- *
- * Be sure to use the trimmed userName for creating the account.
- */
-export const displayNameIsValid = (displayName?: string): string => {
-  if (!displayName) {
-    throw new Error(window.i18n('displayNameErrorDescription'));
-  }
-
-  const trimName = displayName.trim();
-  if (!trimName) {
-    throw new Error(window.i18n('displayNameErrorDescription'));
-  }
-
-  return trimName;
-};
