5dcd0cc6108b8c60ba102cc8ab5f8ee9b48a6f2b
yougotwill <yougotwill94@gmail.com>
2025-03-07 16:31:21 +1100

    fix: include version in update notification message

--- ts/updater/updater.ts
+++ ts/updater/updater.ts
@@ -159,7 +159,11 @@ async function checkForUpdates(
         return;
       }
       logger.info('[updater] showing download dialog...');
-      const shouldDownload = await showDownloadUpdateDialog(mainWindow, i18n);
+      const shouldDownload = await showDownloadUpdateDialog(
+        mainWindow,
+        i18n,
+        result.updateInfo.version
+      );
       logger.info('[updater] shouldDownload:', shouldDownload);
 
       if (!shouldDownload) {
